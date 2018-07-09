e307788fa45f2b2a6aa76838a2616c8b81365705
Tim Taylor <ttaylor@mitre.org>
2018-07-09 15:40:23 -0400

    Merged upstream repo

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -17,6 +17,22 @@
 set -e
 
 if [ "$1" = 'couchdb' ]; then
+	# we need to set the permissions here because docker mounts volumes as root
+	chown -R couchdb:couchdb \
+		/usr/local/var/lib/couchdb \
+		/usr/local/var/log/couchdb \
+		/usr/local/var/run/couchdb \
+		/usr/local/etc/couchdb
+
+	chmod -R 0770 \
+		/usr/local/var/lib/couchdb \
+		/usr/local/var/log/couchdb \
+		/usr/local/var/run/couchdb \
+		/usr/local/etc/couchdb
+
+	chmod 664 /usr/local/etc/couchdb/*.ini
+	chmod 775 /usr/local/etc/couchdb/*.d
+
 	if [ "$COUCHDB_USER" ] && [ "$COUCHDB_PASSWORD" ]; then
 		# Create admin
 		printf "[admins]\n%s = %s\n" "$COUCHDB_USER" "$COUCHDB_PASSWORD" > /usr/local/etc/couchdb/local.d/docker.ini
